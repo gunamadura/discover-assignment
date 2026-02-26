f1a72e4a28048988effd44191f10fc9df6a1317e
gunamadura <gunasekharmadura@gmail.com>
2026-02-26 17:51:26 +0530

    creating dockerfiles for frontend/backend

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://localhost:27017/dd_db"
+  url: "mongodb+srv://gunasekharmadura_db_user:kOpoZNuiyIquKjQ5@discover-assignment.s1rmyhk.mongodb.net/?appName=Discover-Assignment"
 };
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
+
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 // parse requests of content-type - application/json
+app.use(cors()); //
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-
+console.log("hear")
 const db = require("./app/models");
 db.mongoose
   .connect(db.url, {
@@ -31,7 +33,7 @@ app.get("/", (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
